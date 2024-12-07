a74b55fffbea492ca3f70efb559ec1269fdb9f8c
Victor Farias <vfarias@outlook.com>
2024-12-08 02:31:41 +0900

    feat: defined routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,30 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientsController;
+use App\Http\Controllers\StoresController;
+use App\Http\Controllers\TransactionsController;
+use App\Http\Controllers\PointsController;
+use App\Http\Controllers\RulesController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['prefix' => 'clients'], function () {
+    Route::apiResource('/', ClientsController::class);
+});
+
+Route::group(['prefix' => 'stores'], function () {
+    Route::apiResource('/', StoresController::class);
+});
+
+Route::group(['prefix' => 'transactions'], function () {
+    Route::apiResource('/', TransactionsController::class);
+});
+
+Route::group(['prefix' => 'points'], function () {
+    Route::apiResource('/', PointsController::class);
+    Route::get('total', [PointsController::class, 'getTotalPoints']);
+});
+
+Route::group(['prefix' => 'store-rules'], function () {
+    Route::apiResource('/', RulesController::class);
+    Route::get('store/{storeId}', [RulesController::class, 'getRulesByStore']);
+});
