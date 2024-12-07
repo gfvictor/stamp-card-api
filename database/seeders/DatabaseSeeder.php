b394b242945331565ed9a94cca7c129193900498
Victor Farias <vfarias@outlook.com>
2024-12-07 15:41:29 +0900

    chore: set database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Stores;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,10 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Stores::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
+        Stores::factory()->create([
+            'name' => 'Test Stores',
             'email' => 'test@example.com',
         ]);
     }
