ab8c3d30076e6c4af6bebe91dfdd0025f4091892
Victor Farias <vfarias@outlook.com>
2024-12-12 12:09:17 +0900

    refactor: moved each logic to its separated action

--- app/Controllers/StoresController.php
+++ app/Controllers/StoresController.php
@@ -2,66 +2,37 @@
 
 namespace App\Controllers;
 
-use App\Services\StoresService;
-use Illuminate\Http\Response;
+use App\Actions\Store\DestroyAction;
+use App\Actions\Store\IndexAction;
+use App\Actions\Store\ShowAction;
+use App\Actions\Store\StoreAction;
+use App\Actions\Store\UpdateAction;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class StoresController extends Controller
 {
-    protected StoresService $service;
-
-    public function __construct(StoresService $service)
+    public function index(IndexAction $action): JsonResponse
     {
-        $this->service = $service;
+        return $action();
     }
 
-    public function index(): JsonResponse
+    public function store(StoreAction $action): JsonResponse
     {
-        $stores = $this->service->all();
-        return response()->json($stores, 200);
+        return $action(request());
     }
 
-    public function store(Request $request): JsonResponse
+    public function show(ShowAction $action, int $id): JsonResponse
     {
-        $data = $request->validate([
-            'name' => 'required|string|max:50',
-            'email' => 'required|email|unique:stores,email',
-            'password' => 'required|string|min:6',
-            'address' => 'required|string|max:150',
-        ]);
-
-        $data['password'] = bcrypt($data['password']);
-        $store = $this->service->create($data);
-        return response()->json($store, 201);
+        return $action($id);
     }
 
-    public function show($id): JsonResponse
+    public function update(UpdateAction $action, int $id): JsonResponse
     {
-        $store = $this->service->find($id);
-        return response()->json($store, 200);
+        return $action(request(), $id);
     }
 
-    public function update(Request $request, $id): JsonResponse
+    public function destroy(DestroyAction $action, int $id): JsonResponse
     {
-        $data = $request->validate([
-            'name' => 'sometimes|string|max:50',
-            'email' => 'sometimes|email|unique:stores, email',
-            'password' => 'sometimes|string|min:6',
-            'address' => 'sometimes|string|max:150'
-        ]);
-
-        if (isset($data['password'])) {
-            $data['password'] = bcrypt($data['password']);
-        }
-
-        $store = $this->service->update($id, $data);
-        return response()->json($store, 200);
-    }
-
-    public function destroy($id): Response
-    {
-        $this->service->delete($id);
-        return response()->noContent();
+        return $action($id);
     }
 }
